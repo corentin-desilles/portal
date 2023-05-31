64079354fd35a2ff875f9b885a7f9c1f36652985
corentin-desilles <corentindesilles974@gmail.com>
2023-05-31 16:12:37 +0200

    fireflies animation

--- src/script.js
+++ src/script.js
@@ -108,6 +108,7 @@ firefliesGeometry.setAttribute(
 const firefliesMaterial = new THREE.ShaderMaterial({
   transparent: true,
   uniforms: {
+    uTime: { value: 0 },
     uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
     uSize: { value: 100 },
   },
@@ -199,6 +200,9 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update materials
+  firefliesMaterial.uniforms.uTime.value = elapsedTime
 
   // Update controls
   controls.update()
